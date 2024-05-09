00103d5ac7818a156a5c86790234cba7292823e5
Zehra Özer <119448653+Zwhraoz@users.noreply.github.com>
2024-05-09 12:23:13 +0300

    Initial Commit

---
+++ Getir_Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Getir_Clone
+//
+//  Created by zehra özer on 9.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Getir_Clone/Getir_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Getir_CloneApp.swift
+//  Getir_Clone
+//
+//  Created by zehra özer on 9.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Getir_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
